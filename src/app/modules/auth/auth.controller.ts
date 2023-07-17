78f56a4091e49728a3c0fa8f362a595434055f4d
Shihab Ahmed Efty <shihab.ahmed@bizzntek.com>
2023-07-17 22:57:12 +0600

    error response

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-const login = async (req: Request, res: Response) => {
-  return res.status(httpStatus.OK).send({});
-};
+import sendResponse from '../../../shared/sendResponse';
+import { ILoginUserResponse } from './auth.interface';
+import catchAsync from '../../../shared/catchAsync';
+const login = catchAsync(async (req: Request, res: Response) => {
+    // const { ...loginData } = req.body;
+    /* const result = await authService.loginUser(loginData);
+    const { refreshToken, ...others } = result;
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true,
+    };
+  
+    res.cookie('refreshToken', refreshToken, cookieOptions); */
+  
+    sendResponse<ILoginUserResponse>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User logged in successfully',
+      data: null,
+    });
+  });
 export const authController = {
   login,
 };
