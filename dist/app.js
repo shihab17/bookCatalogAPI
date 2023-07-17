9b98cb6c51ce03bc76b2826575e780dd46e5b7ed
Shihab Ahmed Efty <shihab.ahmed@bizzntek.com>
2023-07-17 18:10:47 +0600

    book catalog project initialize

---
+++ dist/app.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_status_1 = __importDefault(require("http-status"));
+/* import globalErrorHandler from './app/middleware/globalErrorHandler';
+import routes from './app/routes'; */
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// app.use('/api/v1', routes);
+app.get('/', (req, res) => {
+    res.send('Book Catalog API ');
+});
+// app.use(globalErrorHandler);
+app.use((req, res, next) => {
+    res.status(http_status_1.default.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API Not Found',
+            },
+        ],
+    });
+    next();
+});
+exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+/* import globalErrorHandler from './app/middleware/globalErrorHandler';
+import routes from './app/routes'; */
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api/v1', routes);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Book Catalog API ');
+});
+// app.use(globalErrorHandler);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+export default app;
